7f79ff42650784bef14b7df161797ceb938df2bd
Mark Bailey <mwbmwb@users.noreply.github.com>
2025-10-14 11:55:08 -0400

    2025-10-14 11:55:08 update

--- Lab6/List.cc
+++ Lab6/List.cc
@@ -3,16 +3,3 @@
 
 using namespace std;
 
-List::Node::Node(const Item& value, Node* prev, Node* next) {
-  _value = value;
-  _prev = prev;
-  _next = next;
-}
-
-List::List() {
-  _head = nullptr;
-  _tail = nullptr;
-  _current = nullptr;
-  _length = 0;
-}
-
