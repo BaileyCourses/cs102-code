2a8ffcba472f35b23959f2f27f29e105a93f1b42
Mark Bailey <mwbmwb@users.noreply.github.com>
2025-11-03 11:58:38 -0500

    2025-11-03 11:58:38 untracked only update

---
+++ lab8/tests.cc
@@ -0,0 +1,12 @@
+#include "catch.hpp"
+#include <sstream>
+#include "List.h"
+
+using namespace std;
+
+TEST_CASE("Lists start empty", "[testA]") {
+  List list;
+
+  CAPTURE(list);
+  REQUIRE(list.length() == 0);
+}
